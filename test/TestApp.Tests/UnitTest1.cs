613f7ce2043975d4b6b26875537ce8960afa7556
Jos van der Til <jos@vandertil.net>
2020-10-15 23:13:57 +0200

    Add test project

---
+++ test/TestApp.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace TestApp.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
